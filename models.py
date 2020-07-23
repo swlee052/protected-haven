210fcd929f0fa4e6e1bb950a1225cabace308c3f
DJTwoTone <DJTwoTone@gmail.com>
2020-07-23 18:35:03 +0900

    refactored db

--- models.py
+++ models.py
@@ -15,18 +15,22 @@ class Lang(db.Model):
     name = db.Column(db.String(), 
                         primary_key=True)
     script = db.Column(db.String(), nullable=False)
-    report = db.Column(db.Integer(), db.ForeignKey('report.id'))
+    form_name = db.Column(db.String(), nullable=True)
+    form_email = db.Column(db.String(), nullable=True)
+    form_phone = db.Column(db.String(), nullable=True)
+    form_detail = db.Column(db.String(), nullable=True)
+    # report = db.Column(db.Integer(), db.ForeignKey('report.id'))
 #   resources = db.Column(db.Integer(), db.ForeignKey('resources.id'))
 
-class Report(db.Model):
-    __tablename__ = 'reports'
-    id = db.Column(db.Integer,
-                    primary_key = True,
-                    autoincrement = True)         
-    name = db.Column(db.String(), nullable=True)
-    email = db.Column(db.String(), nullable=True)
-    phone = db.Column(db.String(), nullable=True)
-    detail = db.Column(db.String(), nullable=True)
+# class Report(db.Model):
+#     __tablename__ = 'reports'
+#     id = db.Column(db.Integer,
+#                     primary_key = True,
+#                     autoincrement = True)         
+#     form_name = db.Column(db.String(), nullable=True)
+#     form_email = db.Column(db.String(), nullable=True)
+#     form_phone = db.Column(db.String(), nullable=True)
+#     form_detail = db.Column(db.String(), nullable=True)
 
 # class Resource(db.Model):
 #      __tablename__ = 'resources'
--- seed.py
+++ seed.py
@@ -5,25 +5,24 @@ db.drop_all()
 db.create_all()
 
 Lang.query.delete()
-Report.query.delete()
+# Report.query.delete()
 
-english = Lang(name='English', script='English')
-chinese = Lang(name='Chinese', script='中文')
-vietnamese = Lang(name='Vietnamese', script='Tiếng Việt')
-tagalog = Lang(name='Tagalog', script='Tagalog')
+english = Lang(name='English', script='English', form_name='Your Name', form_email='Your Email Address', form_phone='Your Phone Number', form_details='Details of the Incident')
+chinese = Lang(name='Chinese', script='中文', form_name='你的名字', form_email='您的电子邮件地址', form_phone='你的电话号码', form_details='事故详情')
+vietnamese = Lang(name='Vietnamese', script='Tiếng Việt', form_name='Tên của bạn', form_email='Địa chỉ email của bạn', form_phone='Số điện thoại của bạn', form_details='Chi tiết về sự cố')
+tagalog = Lang(name='Tagalog', script='Tagalog', form_name='Ang pangalan mo', form_email='Ang iyong email address', form_phone='Iyong numero ng telepono', form_details='Mga Detalye ng Insidente')
 
-db.session.add(english)
-db.session.add(chinese)
-db.session.add(vietnamese)
-db.session.add(tagalog)
 
-db.session.commit()
+db.session.add_all([english, chinese, vietnamese, tagalog])
 
-e_report = Report(name='Your Name', email='Your Email Address', phone='Your Phone Number', details='Details of the Incident')
-c_report = Report(name='你的名字', email='您的电子邮件地址', phone='你的电话号码', details='事故详情')
-v_report = Report(name='Tên của bạn', email='Địa chỉ email của bạn', phone='Số điện thoại của bạn', details='Chi tiết về sự cố')
-t_report = Report(name='Ang pangalan mo', email='Ang iyong email address', phone='Iyong numero ng telepono', details='Mga Detalye ng Insidente')
-
-db.session.add_all([e_report, c_report, v_report, t_report])
 
 db.session.commit()
+
+# e_report = Report(form_name='Your Name', form_email='Your Email Address', form_phone='Your Phone Number', form_details='Details of the Incident')
+# c_report = Report(form_name='你的名字', form_email='您的电子邮件地址', form_phone='你的电话号码', form_details='事故详情')
+# v_report = Report(form_name='Tên của bạn', form_email='Địa chỉ email của bạn', form_phone='Số điện thoại của bạn', form_details='Chi tiết về sự cố')
+# t_report = Report(form_name='Ang pangalan mo', form_email='Ang iyong email address', form_phone='Iyong numero ng telepono', form_details='Mga Detalye ng Insidente')
+
+# db.session.add_all([e_report, c_report, v_report, t_report])
+
+# db.session.commit()
