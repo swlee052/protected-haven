07cd7eedfc34320e4b55aeb178a082302b8a8ce0
Ebipatei <65502175+Ebipatei@users.noreply.github.com>
2020-08-03 16:19:15 +0900

    Update seed.py

--- seed.py
+++ seed.py
@@ -1,12 +1,15 @@
 from models import db, Lang#, Report
 from app import app
 
+#(comment needed)
 db.drop_all()
 db.create_all()
 
+#(comment needed)
 Lang.query.delete()
 # Report.query.delete()
 
+#(comment needed)
 english = Lang(name='English', script='English', form_name='Your Name', form_email='Your Email Address', form_phone='Your Phone Number', form_details='Details of the Incident')
 chinese = Lang(name='Chinese', script='中文', form_name='你的名字', form_email='您的电子邮件地址', form_phone='你的电话号码', form_details='事故详情')
 vietnamese = Lang(name='Vietnamese', script='Tiếng Việt', form_name='Tên của bạn', form_email='Địa chỉ email của bạn', form_phone='Số điện thoại của bạn', form_details='Chi tiết về sự cố')
