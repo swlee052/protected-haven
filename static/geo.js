ada30d24dcd3733ccf2af690770d69a41904dd52
Sungwoo Lee <swjj0320@naver.com>
2020-08-07 19:02:01 +0900

    fixed geo.js

--- static/geo.js
+++ static/geo.js
@@ -1,30 +1,27 @@
-window.onload = function() {
-  
+function find_loc(){
   const options = {
     timeout: 5000,
-    maximumAge: 0
+    maximumAge: 0,
+    enableHighAccuracy: true
   };
-  
-  function loc_success(pos) {
-    var crd = pos.coords;
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-    return crd;
-  }
-  
-  function loc_error(err) {
-    return 'Location not included';
-  }
-  
-  function find_loc(){
-    loc = navigator.geolocation.getCurrentPosition(loc_success, loc_error, options);
-    console.log(loc);
-    return loc;
-  }
-  find_loc()
-}
 
+  function loc_success(pos) {
+    const crd = pos.coords;
+    //console.log('Your current position is:');
+    //console.log(`Latitude : ${crd.latitude}`);
+    //console.log(`Longitude: ${crd.longitude}`);
+    //console.log(`More or less ${crd.accuracy} meters.`);
+    find_loc.latitude = crd.latitude;
+    find_loc.longitude = crd.longitude;
+    find_loc.accuracy = crd.accuracy;
+  }
+
+  function loc_error(err) {
+    find_loc.error = 'Location not included';
+  }
+
+  navigator.geolocation.getCurrentPosition(loc_success, loc_error, options);
+  return find_loc;
+}
 
 // window.onload = find_loc();
