b0153a357098614a30078c6e865f9107f2ae3fc6
Sungwoo Lee <swjj0320@naver.com>
2020-08-22 17:06:27 +0900

    seed.py update

--- seed.py
+++ seed.py
@@ -1,5 +1,9 @@
 from models import db, Lang, Resource
 from app import app
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 #resets, then creates our basic database
 db.drop_all()
